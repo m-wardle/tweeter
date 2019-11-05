f7770a6630b685cedadbb8f61c4e6e0ccdd3682e
Matthew Wardle <mwardle.22@gmail.com>
2019-11-05 21:48:52 +0000

    Implemented proper selective hover behaviour for tweets

--- public/scripts/tweets.js
+++ public/scripts/tweets.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   // --- our code goes here ---
   $("article.tweet").hover(function() {
-    $("article.tweet").css("box-shadow", "10px 10px #bdbdbd");
-    $("p.tweet-handle").css("visibility", "visible");
+    $(this).css("box-shadow", "10px 10px #bdbdbd");
+    $(this).find("p.tweet-handle").css("visibility", "visible");
   }, function() {
-    $("article.tweet").css("box-shadow", "none");
-    $("p.tweet-handle").css("visibility", "hidden");
+    $(this).css("box-shadow", "none");
+    $(this).find("p.tweet-handle").css("visibility", "hidden");
   })
 });
