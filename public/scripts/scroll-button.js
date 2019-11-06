50eef5a1e72fa2e1936b89859a34269e2ceeeec0
Matthew Wardle <mwardle.22@gmail.com>
2019-11-06 22:13:41 +0000

    Implement scroll to top button. Temporarily removed toggle functionality from top scroll toggle button due to some redundancy/conflicts with new instructions.

--- public/scripts/scroll-button.js
+++ public/scripts/scroll-button.js
@@ -6,16 +6,16 @@ const scrollToggle = () => {
       scrollTop: $(".new-tweet").offset().top - 150
     }, 1000);
     $(".new-tweet textarea").focus();
-    toggle = 1;
-  } else {
-    $('html, body').animate({
+    // toggle = 1; // With implementation of scroll to top button this seems a bit pointless.
+  } else { 
+    $('html, body').animate({ 
       scrollTop: $("#tweets-container").offset().top - 150
     }, 1000);
     toggle = 0;
   }
 }
 
-const hover = () => {
+const scrollHover = () => {
   $("#scroll-toggle").hover(function() {
     $(this).css("cursor", "pointer");
   }, function() {
@@ -23,9 +23,30 @@ const hover = () => {
   })
 }
 
+const scrollButton = () => {
+  const btn = $('#scroll-top-btn');
+
+  $(document).scroll(function() {
+    if ($(document).scrollTop() > 20) {
+      btn.css('display', 'block');
+      $('#scroll-toggle').css('display', 'none');
+    } else {
+      btn.css('display', 'none');
+      $('#scroll-toggle').css('display', 'block');
+    }
+  });
+
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+  });
+}
+
+
 $(document).ready(function() {
   $("#scroll-toggle").on("click", () => {
     scrollToggle();
   })
-  hover();
+  scrollHover();
+  scrollButton();
 });
