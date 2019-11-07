1bdf4e761cfd0a30b3e032109b974ea5f748da26
Matthew Wardle <mwardle.22@gmail.com>
2019-11-07 22:40:06 +0000

    Fix implementation of scroll toggle/loading per final requirements

--- public/scripts/scroll-button.js
+++ public/scripts/scroll-button.js
@@ -2,15 +2,29 @@ let toggle = 0
 
 const scrollToggle = () => {
   if (toggle === 0) {
-    $('html, body').animate({
-      scrollTop: $(".new-tweet").offset().top - 150
-    }, 1000);
+    $(".new-tweet").show(500);
+    // if ($(document).width() < 1024) {
+    //   $('html, body').animate({
+    //     scrollTop: $(".new-tweet").offset().top - 150
+    //   }, 1000);
+    // } else {
+    //   $('html, body, .container').animate({
+    //     scrollTop: $("#tweets-container").offset().top - 150
+    //   }, 1000);
+    // }
     $(".new-tweet textarea").focus();
-    // toggle = 1; // With implementation of scroll to top button this seems a bit pointless.
+    toggle = 1; 
   } else { 
-    $('html, body').animate({ 
-      scrollTop: $("#tweets-container").offset().top - 150
-    }, 1000);
+    $(".new-tweet").hide(500);
+    // if ($(document).width() < 1024) {
+    //   $('html, body').animate({ 
+    //     scrollTop: $("#tweets-container").offset().top - 105
+    //   }, 1000);
+    // } else {
+    //   $('html, body, .container').animate({
+    //     scrollTop: $(".new-tweet").offset().top - 70
+    //   }, 1000);
+    // }
     toggle = 0;
   }
 }
@@ -27,7 +41,7 @@ const scrollButton = () => {
   const btn = $('#scroll-top-btn');
 
   $(document).scroll(function() {
-    if ($(document).scrollTop() > 20) {
+    if ($(document).scrollTop() > 550) {
       btn.css('visibility', 'visible');
       $('#scroll-toggle').css('visibility', 'hidden');
     } else {
@@ -37,7 +51,7 @@ const scrollButton = () => {
   });
 
   $("main.container").scroll(function() {
-    if ($("main.container").scrollTop() > 20) {
+    if ($("main.container").scrollTop() > 105) {
       btn.css('visibility', 'visible');
       $('#scroll-toggle').css('visibility', 'hidden');
     } else {
@@ -55,6 +69,13 @@ const scrollButton = () => {
 
 
 $(document).ready(function() {
+  // if ($(document).width() < 1024) {
+  //   $("#tweets-container").scrollTop
+  // } else {
+  //   $('html, body, .container').animate({
+  //     scrollTop: $(".new-tweet").offset().top - 70
+  //   }, 1000);
+  // }
   $("#scroll-toggle").on("click", () => {
     scrollToggle();
   })
